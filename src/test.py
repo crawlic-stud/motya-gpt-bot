966bee3156629b706b53854e664203af588f4dd1
crawlic-stud <nikitosik0726@gmail.com>
2023-04-11 10:27:37 +0300

    updated tests

--- src/test.py
+++ src/test.py
@@ -1,13 +1,13 @@
 import os
 
 from dotenv import load_dotenv
+import pytest
 
-from model import MotyaModel
+from async_model import AsyncMotyaModel
 from mongo import ConfigDb
 
 
 load_dotenv()
-motya = MotyaModel()
 config_db = ConfigDb(os.getenv("MONGO_URL"), "motya_gpt", "config")
 
 
@@ -16,10 +16,12 @@ def test_getting_themes():
     assert themes is not None, "No themes for a new post"
 
 
-def test_creates_random_post():
+@pytest.mark.asyncio
+async def test_creates_random_post():
+    motya = await AsyncMotyaModel.create()
     themes = config_db.get_themes()
-    assert isinstance(motya.create_random_post(
-        themes), str), "Must be a string!"
+    post = await motya.create_random_post(themes)
+    assert isinstance(post, str), "Must be a string!"
 
 
 if __name__ == "__main__":
