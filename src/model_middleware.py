6978795cac0ec3320d1cce6610826af7a91088ae
crawlic-stud <nikitosik0726@gmail.com>
2023-10-14 16:00:51 +0300

    remove mindsdb_model

--- src/model_middleware.py
+++ src/model_middleware.py
@@ -2,13 +2,13 @@ from aiogram.dispatcher.handler import ctx_data
 from aiogram import types
 from aiogram.dispatcher.middlewares import BaseMiddleware
 
-from model.mindsdb_model import MindsDbMotyaModel
+from model.g4f_model import GPT4FreeModel
 
 
 class ModelMiddleware(BaseMiddleware):
-    def __init__(self, model: MindsDbMotyaModel) -> None:
+    def __init__(self, model: GPT4FreeModel) -> None:
         super().__init__()
         self.model = model
 
-    async def on_pre_process_message(self, message: types.Message, data: dict):
+    async def on_pre_process_message(self, _: types.Message, data: dict):
         data["model"] = self.model
